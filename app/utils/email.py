367ebada5e9ebbebea8c87ec98850ed2b1cdae90
thinh <thinhk2019dthb1@gmail.com>
2025-05-03 16:44:56 +0700

    update file email resend

--- app/utils/email.py
+++ app/utils/email.py
@@ -8,7 +8,7 @@ resend.api_key = os.getenv("RESEND_API_KEY")
 
 def send_otp_email(to_email: str, otp_code: str):
     return resend.Emails.send({
-        "from": "onboarding@resend.dev",  # Bạn có thể thay đổi nếu đã xác minh domain riêng
+        "from": "thinhtop869@gmail.com",  # Bạn có thể thay đổi nếu đã xác minh domain riêng
         "to": [to_email],
         "subject": "CMMS - Mã OTP đặt lại mật khẩu",
         "html": f"<p>Mã OTP của bạn là: <strong>{otp_code}</strong></p>"
